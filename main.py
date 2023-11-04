253af99e8d84dae8176897efe0f1d072add7fc58
Jonathanctyyy <zdqh62@durham.ac.uk>
2023-11-04 22:10:04 +0000

    adding budget

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from taipy.gui import Gui, notify, navigate
+from taipy.gui import Gui, notify, navigate, Markdown
 from pages.index import index_md
 from pages.budget import budget_md
 from pages.categories import categories_md
 from pages.academy import academy_md
 from pages.settings import settings_md
+import pandas as pd
 
 text = "Original text"
 
@@ -33,6 +34,7 @@ def on_change(state, var_name, var_value):
 def on_menu(state, action, payload):
     page = payload["args"][0]
     navigate(state, page)
+
 
 
 Gui(pages=pages).run(run_browser=False, use_reloader=True, port=3000)
--- pages/budget.py
+++ pages/budget.py
@@ -1,1 +1,20 @@
-budget_md = "## Your Budget"
+from taipy.gui import Gui, notify, navigate, Markdown
+import pandas as pd
+
+budget_md = Markdown("<|{spending_df}|table|filter=True|on_add=spending_df_on_add|>")
+
+priceList = [9.84, 20.00, 60.00, 7.24, 2.61, 8.78, 4.35, 10.00, 5.17, 9.83]
+spending_df = pd.DataFrame({
+    "Category": ["Expense", "Savings", "Income", "Expense", "Expense", "Expense", "Expense", "Savings", "Expense", "Expense"],
+    "Item": ["food", "part time job", "Parents", "textbook", "leisure", "textbook", "transportation", "part time job", "food", "tutoring"],
+    "price": [f'£{price:.2f}' for price in priceList],
+    "total balance": ['-', '+', '+', '-', '-', '+', '-', '+', '+', '-'],
+})
+
+sum = sum(priceList)
+
+def spending_df_on_add(state, var_name, payload):
+    empty_row = pd.DataFrame([[None for _ in state.spending_df.columns]], columns=state.spending_df.columns)
+    state.spending_df = pd.concat([empty_row, state.spending_df], axis=0, ignore_index=True)
+
+    notify(state, "S", f"Added a new row.")
