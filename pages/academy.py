248bfd3517d727301dbfc4b0b618109167d4be54
Jonathanctyyy <zdqh62@durham.ac.uk>
2023-11-05 08:32:58 +0000

    added academy and modified settings

--- pages/academy.py
+++ pages/academy.py
@@ -1,1 +1,16 @@
-academy_md = "## Your Academy"
+academy_md = """
+<h1> Your Score: </h1> <p> </p>
+<|{score}|>
+<p> </p>
+Expense: <|{expense}|> <p> </p>
+Budget: <|{budget}|> <p> </p>
+"""
+expense = 700
+budget = 600
+
+def score():
+    if expense/budget >= 0.8:
+        return 100
+    else:
+      return 100^(1.8-expense/budget)
+    
