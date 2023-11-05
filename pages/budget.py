4de78780f534b9e1205f935b5faab090654d7c3a
Jonathanctyyy <zdqh62@durham.ac.uk>
2023-11-05 00:46:16 +0000

    adding final category

--- pages/budget.py
+++ pages/budget.py
@@ -1,20 +1,33 @@
 from taipy.gui import Gui, notify, navigate, Markdown
 import pandas as pd
 
-budget_md = Markdown("<|{spending_df}|table|filter=True|on_add=spending_df_on_add|>")
+budget_md = page = """
+<|toggle|theme|>
 
-priceList = [9.84, 20.00, 60.00, 7.24, 2.61, 8.78, 4.35, 10.00, 5.17, 9.83]
-spending_df = pd.DataFrame({
-    "Category": ["Expense", "Savings", "Income", "Expense", "Expense", "Expense", "Expense", "Savings", "Expense", "Expense"],
-    "Item": ["food", "part time job", "Parents", "textbook", "leisure", "textbook", "transportation", "part time job", "food", "tutoring"],
-    "price": [f'£{price:.2f}' for price in priceList],
-    "total balance": ['-', '+', '+', '-', '-', '+', '-', '+', '+', '-'],
-})
+# Getting started with Taipy GUI
 
-sum = sum(priceList)
+<|layout|columns=1 1|
+<|
+My text: <|{text}|>
 
-def spending_df_on_add(state, var_name, payload):
-    empty_row = pd.DataFrame([[None for _ in state.spending_df.columns]], columns=state.spending_df.columns)
-    state.spending_df = pd.concat([empty_row, state.spending_df], axis=0, ignore_index=True)
+Enter a word:
+<|{text}|input|>
+<|Analyze|button|on_action=local_callback|>
+|>
 
-    notify(state, "S", f"Added a new row.")
+
+<|Table|expandable|
+<|{dataframe}|table|width=100%|>
+|>
+|>
+
+<|layout|columns=1 1 1|
+## Positive <|{np.mean(dataframe['Score Pos'])}|text|format=%.2f|raw|>
+
+## Neutral <|{np.mean(dataframe['Score Neu'])}|text|format=%.2f|raw|>
+
+## Negative <|{np.mean(dataframe['Score Neg'])}|text|format=%.2f|raw|>
+|>
+
+<|{dataframe}|chart|type=bar|x=Text|y[1]=Score Pos|y[2]=Score Neu|y[3]=Score Neg|y[4]=Overall|color[1]=green|color[2]=grey|color[3]=red|type[4]=line|>
+"""
